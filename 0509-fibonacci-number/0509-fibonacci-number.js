976541e9d1715f67bba2c6f67d5993174f668ecd
Pranay Kumar <122789579+prrranay@users.noreply.github.com>
2026-06-29 00:01:53 +0530

    Time: 41 ms (82.86%), Space: 52.8 MB (89.68%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.js
+++ 0509-fibonacci-number/0509-fibonacci-number.js
@@ -3,11 +3,16 @@
  * @return {number}
  */
 var fib = function(n) {
-    if(n==0){
-        return 0;
-    }
-    if(n==1){
-        return 1;
-    }
-    return fib(n-1)+fib(n-2);
+    if(n<2) return n;
+    let arr=Array(n+1).fill(-1);
+    arr[0]=0;
+    arr[1]=1;
+    return solve(n,arr);
 };
+
+function solve(n,arr){
+    if(arr[n] == -1){
+        arr[n]=solve(n-1,arr)+solve(n-2,arr);
+    }
+    return arr[n];
+}
