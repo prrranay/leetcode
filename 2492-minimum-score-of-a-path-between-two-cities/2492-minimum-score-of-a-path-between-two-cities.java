6157cdd7a461a7aa552c5d39624464d400d3e47f
Pranay Kumar <122789579+prrranay@users.noreply.github.com>
2026-07-04 18:24:56 +0530

    Time: 98 ms (8.77%), Space: 163.5 MB (38.82%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.java
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.java
@@ -1,29 +1,29 @@
 class Solution {
     public int minScore(int n, int[][] roads) {
-        int ans=Integer.MAX_VALUE;
-        
-        List<List<int[]>> graph=new ArrayList<>();
-        
-        for(int i=0;i<=n;i++){
+        int ans = Integer.MAX_VALUE;
+
+        List<List<int[]>> graph = new ArrayList<>();
+
+        for (int i = 0; i <= n; i++) {
             graph.add(new ArrayList<>());
         }
-        
-        for(int [] road:roads){
-            graph.get(road[1]).add(new int[]{road[0],road[2]});
-            graph.get(road[0]).add(new int[]{road[1],road[2]});
+
+        for (int[] road : roads) {
+            graph.get(road[1]).add(new int[] { road[0], road[2] });
+            graph.get(road[0]).add(new int[] { road[1], road[2] });
         }
 
-        Stack<Integer> stack=new Stack<>();
-        boolean[] vis=new boolean[n+1];
+        Stack<Integer> stack = new Stack<>();
+        boolean[] vis = new boolean[n + 1];
         stack.push(1);
-        
-        while(!stack.isEmpty()){
-            int node=stack.pop();
-            vis[node]=true;
-            
-            for(int[] next:graph.get(node)){
-                ans=Math.min(ans,next[1]);
-                if(!vis[next[0]]){
+        vis[1] = true;
+        while (!stack.isEmpty()) {
+            int node = stack.pop();
+
+            for (int[] next : graph.get(node)) {
+                ans = Math.min(ans, next[1]);
+                if (!vis[next[0]]) {
+                    vis[node] = true;
                     stack.push(next[0]);
                 }
             }
