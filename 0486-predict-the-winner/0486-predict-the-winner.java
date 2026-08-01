a96827aaf1b1d8a39b8c34ea91eab297b2e09b76
Pranay Kumar <122789579+prrranay@users.noreply.github.com>
2026-08-01 22:52:11 +0530

    Time: 0 ms (100%), Space: 42.7 MB (74.96%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -3,16 +3,12 @@ class Solution {
         int n = nums.length;
         if ((n & 1) == 0) return true;
 
-        int[][] dp = new int[n][n];
-        for (int[] r : dp) {
-            Arrays.fill(r, -1);
-        }
-
+        Integer[][] dp = new Integer[n][n];
         return maxDiff(0, n - 1, nums, dp) >= 0;
     }
 
-    int maxDiff(int i, int j, int[] nums,int[][] dp){
-        if(dp[i][j] != -1) return dp[i][j];
+    int maxDiff(int i, int j, int[] nums,Integer[][] dp){
+        if(dp[i][j] != null) return dp[i][j];
         if(i == j) return dp[i][j] =nums[i];
 
         return dp[i][j]= Math.max(nums[i]-maxDiff(i+1,j,nums,dp),nums[j] - maxDiff(i,j-1,nums,dp));
