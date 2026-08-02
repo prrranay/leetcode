8d5a523c7aa2e03ef9498905ca01e9d416102618
Pranay Kumar <122789579+prrranay@users.noreply.github.com>
2026-08-02 12:20:50 +0530

    Time: 0 ms (100%), Space: 43.3 MB (35.84%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,15 +1,5 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-         int n = piles.length;
-        if ((n & 1) == 0) return true;
-
-        Integer[][] dp = new Integer[n][n];
-        return maxDiff(0, n - 1, piles, dp) >= 0;
-    }
-    int maxDiff(int i, int j, int[] nums,Integer[][] dp){
-        if(dp[i][j] != null) return dp[i][j];
-        if(i == j) return dp[i][j] =nums[i];
-
-        return dp[i][j]= Math.max(nums[i]-maxDiff(i+1,j,nums,dp),nums[j] - maxDiff(i,j-1,nums,dp));
+        return true;
     }
 }
