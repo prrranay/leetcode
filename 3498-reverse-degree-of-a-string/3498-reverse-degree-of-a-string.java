8cb0a104d946a9d393b1584ef6c4908660c63a76
Pranay Kumar <122789579+prrranay@users.noreply.github.com>
2026-09-20 12:47:26 +0530

    Time: 1 ms (100%), Space: 44.1 MB (50.47%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int reverseDegree(String s) {
+        int ans=0;
+        for(int i=0;i<s.length();i++){
+            int pos='z'-s.charAt(i)+1;
+            ans+=(pos*(i+1));
+        }
+        return ans;
+    }
+}
